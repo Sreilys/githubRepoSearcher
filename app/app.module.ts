503c9c1aafdc320d692b9c549a15d5946d25d8a0
Sabrina Bosco <sabrina@wonderweblabs.io>
2016-11-24 21:29:56 +0100

    create and import profile component

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
 
+import { ProfileComponent } from './components/profile.component';
+
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [ BrowserModule ],
+  declarations: [ AppComponent, ProfileComponent ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
