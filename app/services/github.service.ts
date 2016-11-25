49ed2f29afb2abb0827c3c68111d57e2fddc6c87
Sabrina Bosco <sabrina@wonderweblabs.io>
2016-11-25 09:25:38 +0100

    create github service for use client id and secret from github application and display user object in console

---
+++ app/services/github.service.ts
@@ -0,0 +1,22 @@
+import {Injectable} from '@angular/core';
+import {Http, Headers} from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class GithubService {
+
+  private username:string;
+  private client_id:'ea03ffe8c783382534ce';
+  private client_secret:'76e3ff579b4b46ea57a14086ec44c2c9b3af269d';
+
+  constructor(private _http: Http) {
+    console.log('Github Service Read!');
+    this.username = 'Sabrina';
+  }
+
+  getUser() {
+    return this._http.get('http://api.github.com/users/' + this.username + '?client_id=' + this.client_id + '&client_secret=' + this.client_secret)
+                     .map(res => res.json());
+  }
+
+}
